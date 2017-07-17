542c21fe2f9168275edd20e1bc4078317c0e7e65
dkw <dkw@renre.com>
2017-07-17 16:03:21 -0300

    Added spreadsheet view demo

---
+++ www/js/spreadsheet_view/src/js/SpreadSheet.js
@@ -0,0 +1,66 @@
+"use strict";
+
+function SpreadSheet(selector) {
+    this.d3element = d3.select(selector);
+    this.refresh();
+}
+
+SpreadSheet.prototype.onSelect = function(toSelect, toRemove, undefined) {
+    alert("Clicked " + toSelect);
+}
+
+SpreadSheet.prototype.refresh = function() {
+    var handle_selection = this.onSelect;
+    var element = this.d3element.node();
+
+    ajaxFunction(
+        "/api/getdata",
+
+        function(ajax_result) {
+            var descriptors = ajax_result.result;
+            var rows = [];
+
+            var columns = [
+                "name",
+                "value",
+            ];
+
+            for (var i = 0; i < descriptors.length; ++i) {
+                var descriptor = descriptors[i];
+                var row = [];
+
+                row.push(descriptor.name);
+                row.push(descriptor.value);
+                rows.push(row);
+            }
+
+            var grid_contents = {
+                Head: [columns],
+                Body: rows
+            };
+
+            var grid_reference = new Grid(
+                element, {
+                    srcType: "json", 
+                    srcData: grid_contents, 
+                    allowGridResize: true, 
+                    allowColumnResize: true, 
+                    allowClientSideSorting: true, 
+                    allowSelections: true, 
+                    allowMultipleSelections: false, 
+                    showSelectionColumn: false, 
+                    fixedCols: 0,
+                    onRowSelect: handle_selection,
+                }
+            );
+        },
+
+        function(ajax_exception) {
+        },
+
+        "GET",
+        null
+    );
+}
+
+window.SpreadSheet = SpreadSheet;
