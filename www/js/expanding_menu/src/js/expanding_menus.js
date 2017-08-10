6e62ed8458a2b1cae9becbfe6648e44d07577989
damionw <damionw@gmail.com>
2017-08-10 00:24:14 -0300

    incomplete changes to menu demo

--- www/js/expanding_menu/src/js/expanding_menus.js
+++ www/js/expanding_menu/src/js/expanding_menus.js
@@ -1,38 +1,32 @@
-var Supervisor = {
-    refresh: function() {
-        var supervisor_reference = this;
-
-        ajaxFunction(
-            "/api/status",
-
-            function(ajax_result){
-                supervisor_reference.formatData(ajax_result);
-                supervisor_reference.onDataReceived();
-            },
-
-            function(ajax_exception){
-            }
-        );
-    },
-
-    formatData: function(data) {
-        this.tasks = new Object();
-
-        for (var index = 0; index < data.tasks.length; ++index) {
-            this.tasks[data.tasks[index].name] = data.tasks[index];
-        }
-
-        this.dependencies = data.dependencies;
-        this.name = data.name;
-        this.pid = data.supervisor;
-        this.folder = data.suite;
-    },
-
-    onDataReceived: function() {
-        console.log("RECEIVED: " + Object.keys(this.tasks).join(", "));
-        this.updateTasks();
-    },
-
-    refreshTasks() {
+var _Expanding = window.Expanding = function(selector) {
+    var format_element = function() {
     }
-};
+
+    var format_container = function() {
+        var container = this;
+
+        var children = d3.selectAll(d3.select(container).node().childNodes)
+            .filter(
+                function() {
+                    return this.nodeName == "DIV";
+                }
+            )
+        ;
+
+        children
+            .style("text-align", "center")
+            .style("border-style", "ridge")
+            .style("border-color", "white")
+            .style("border-width", "0px 0px 2px 0px")
+            .each(format_element)
+        ;
+
+        d3.select(children[0])
+            .style("border-width", "2px 0px 2px 0px")
+        ;
+    };
+
+    d3.selectAll(selector)
+        .each(format_container);
+    ;
+}
