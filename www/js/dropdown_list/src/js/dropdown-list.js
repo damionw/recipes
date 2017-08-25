2c57e39ac1e4d5cc1a371ca2e6f2483335a28af1
dkw <dkw@renre.com>
2017-08-25 10:42:37 -0300

    working demo

--- www/js/dropdown_list/src/js/dropdown-list.js
+++ www/js/dropdown_list/src/js/dropdown-list.js
@@ -1,68 +1,34 @@
-var _Expanding = window.Expanding = function(selector) {
-    var style_class = "_expanding";
+function refresh() {
+    ajaxFunction(
+        "/api/testdata",
 
-    var style_rules = [
-        "." + style_class + ` {
-            text-align: center;
-            border-style: ridge;
-            border-color: white;
-            border-width: 0px 0px 2px 0px;
-            margin: 5px 0px 5px 0px;
-            cursor: pointer;
-        }`,
+        function(ajax_result) {
+            d3.select(".selection")
+                .selectAll("div")
+                .data(ajax_result)
+                .enter()
+                    .append("div")
+                    .classed("option", true)
+                    .classed("dropdown-option", true)
+                    .text(function(d,i) {return d;})
+                    .on("click", function(d) {select(d);})
+            ;
+        },
 
-        "." + style_class + `:hover {
-            color: #ffffff;
-        }`
-    ];
+        function(ajax_exception) {
+            console.log("Cannot retrieve values");
+        },
 
-    var div_filter = function(d, i) {
-        return this.nodeName == "DIV";
-    };
+        "GET",
 
-    var first_filter = function(d, i) {
-        return i == 0;
-    };
+        null
+    );
+}
 
-    var format_element = function() {
-    };
+function select(name) {
+    console.log(name);
 
-    var add_style = function() {
-        // Create a new style tag
-        var style = document.createElement("style");
-
-        // WebKit hack
-        style.appendChild(document.createTextNode(""));
-
-        // Append the style tag to head
-        document.head.appendChild(style);
-
-        // Grab the stylesheet object
-        var sheet = style.sheet;
-
-        // Use addRule or insertRule to inject styles
-        for (var i = 0; i < style_rules.length; ++i) {
-            sheet.insertRule(style_rules[i], i);
-        }
-    };
-
-    var format_container = function() {
-        var children = d3.selectAll(d3.select(this).node().childNodes).filter(div_filter);
-
-        children
-            .classed(style_class, true)
-            .each(format_element)
-        ;
-
-        children
-            .filter(first_filter)
-            .style("border-width", "2px 0px 2px 0px")
-        ;
-    };
-
-    add_style();
-
-    d3.selectAll(selector)
-        .each(format_container);
+    d3.select(".name")
+        .text(name)
     ;
 }
