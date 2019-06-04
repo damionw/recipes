f870d1e0d02cde567d9adbf3b34f96f91eace456
damionw <damionw@gmail.com>
2019-06-04 00:06:27 -0300

    changes

--- www/vuejs/basic_demo/src/static/basic_demo.js
+++ www/vuejs/basic_demo/src/static/basic_demo.js
@@ -1,17 +1,51 @@
 var settings = {
     el: '#app',
+    
+    data: {
+        drawer: false,
+
+        application: {
+            description: "Basic Demo",
+        },
+    },
+};
+
+var my_folder = {
+    template: "#my-folder",
+
+    props: [
+    ],
+
+    data(){
+        return {
+        }
+    },
+
+    methods: {
+        handle_folder_event: function() {
+        }
+    },
+
+    watch: {
+    }
 };
 
 document.addEventListener(
     'DOMContentLoaded',
 
     function() {
+        settings.components = [
+            Vue.component("my-folder", my_folder),
+        ];
+
+        Vue.use(
+            Vuetify, {
+                iconfont: 'mdi',
+            }
+        );
+
         new Vue(
             settings,
         );
-        
-        Vue.use(Vuetify);
-    },
-
-    false
+    }
 );
