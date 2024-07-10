7b747b02a391c2f55e4dbe3e888f66d88ee98629
Damion K. Wilson <damionw@gmail.com>
2024-07-09 23:37:19 -0300

    changes

---
+++ www/youeye.js/playpen/playpen.js
@@ -0,0 +1,80 @@
+function main_menu_handler(topic, payload) {
+    this.setAttribute(
+        "show", (
+            topic == "menu_hide" || (
+                topic == "menu_toggle" && this.getAttribute("show") == "true"
+            ) ?
+            "false" :
+            "true"
+        )
+    );
+}
+
+function tabular_event(topic, payload) {
+    switch(topic) {
+        case "initialize_table":
+            const column_definitions = [
+                {title:"Name", field: "name"},
+                {title:"Phone", field: "phone", hozAlign:"left"}
+            ];
+
+            var tabulator = this._tabulator = new Tabulator(
+                this, {
+                    data: [], //assign data to table
+                    layout: "fitDataStretch", //"fitDataStretch"
+                    //                 responsiveLayout: "collapse",
+                    selectable: 1, //make 1 row selectable at a time
+                    columns: column_definitions
+                }
+            );
+
+            tabulator.on(
+                "rowSelected", // "rowSelectionChanged",
+
+                function(row_object){
+                    pubsubMessageRouter.singleton.emit(
+                        "table_row_selected",
+                        row_object.getData()
+                    );
+                }
+            );
+
+//  DEBUG
+
+            // Must delay until the table is "ready" to accept data
+            setTimeout(
+                () => {
+                    pubsubMessageRouter.singleton.emit(
+                        "load_table", [
+                            {
+                                "name": "Me",
+                                "phone": "123-4567"
+                            },
+                            {
+                                "name": "Them",
+                                "phone": "234-7777"
+                            },
+                            {
+                                "name": "You",
+                                "phone": "999-9999"
+                            }
+                        ]
+                    );
+                },
+
+                500
+            );
+//  DEBUG
+
+            break;
+
+        case "load_table":
+            var tabulator = this._tabulator;
+
+            if (tabulator != null) {
+                tabulator.replaceData(payload);
+            }
+
+            break;
+    }
+}
